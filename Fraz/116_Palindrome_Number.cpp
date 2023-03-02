196060cada3401ab2bcb08c6c9806e50b8e781c1
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-03-02 22:59:06 +0530

    Create 116_Palindrome_Number.cpp

---
+++ Fraz/116_Palindrome_Number.cpp
@@ -0,0 +1,19 @@
+/*
+  Platform : Leetcode
+  Problem : https://leetcode.com/problems/palindrome-number/
+*/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int temp = x;
+        long long rev = 0;
+        
+        while(temp) {
+            rev = (rev*10) + (temp%10);
+            temp /= 10;
+        }
+        
+        return (x==rev && x>=0) ? true : false;
+    }
+};
