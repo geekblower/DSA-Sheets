ab9ac8950bc7441707bdb1cffd40bd81c02449d8
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-03-07 00:06:45 +0530

    Create 028_Valid_Anagram.cpp

---
+++ Apna College/028_Valid_Anagram.cpp
@@ -0,0 +1,14 @@
+/*
+  Platform : Leetcode
+  Problem : https://leetcode.com/problems/valid-anagram/
+*/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        
+        return (s==t) ? true : false;
+    }
+};
