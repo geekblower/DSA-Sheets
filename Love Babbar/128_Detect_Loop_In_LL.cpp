4b66d0687a22c30d7f95626acd2c15bfb183bff2
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-04-18 22:53:27 +0530

    Create 128_Detect_Loop_In_LL.cpp

---
+++ Love Babbar/128_Detect_Loop_In_LL.cpp
@@ -0,0 +1,30 @@
+/*
+  Platform : Leetcode
+  Problem : https://leetcode.com/problems/linked-list-cycle/
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        map<ListNode*, bool> visited;
+        
+        while(head != NULL) {
+            if(visited[head] == true) {
+                return true;
+            } else {
+                visited[head] = true;
+                head = head->next;
+            }
+        }
+        
+        return false;
+    }
+};
