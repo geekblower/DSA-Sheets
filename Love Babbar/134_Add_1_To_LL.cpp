45a02fcf65e68912fb2d77291da8abea210fc7c3
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-04-18 23:26:01 +0530

    Update 134_Add_1_To_LL.cpp

--- Love Babbar/134_Add_1_To_LL.cpp
+++ Love Babbar/134_Add_1_To_LL.cpp
@@ -3,19 +3,6 @@
   Problem : https://practice.geeksforgeeks.org/problems/add-1-to-a-number-represented-as-linked-list/1
 */
 
-struct Node
-{
-    int data;
-    struct Node* next;
-    
-    Node(int x){
-        data = x;
-        next = NULL;
-    }
-};
-
-*/
-
 class Solution {
     private:
     void insertHead(Node* &head, int data) {
